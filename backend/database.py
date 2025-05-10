fa5012fb5c89fe9241788ab895169f4f658c173b
naidat21 <tahirovicnaida4@gmail.com>
2025-05-10 11:38:53 +0200

    database i requirements update

---
+++ backend/database.py
@@ -0,0 +1,16 @@
+from sqlmodel import create_engine
+#ovaj deo nije gootv mora da se spoji na bazu!!
+
+
+# Podaci za konekciju
+DATABASE_USER = "postgres"  # tvoje korisničko ime
+DATABASE_PASSWORD = "lozinka"  # tvoja lozinka
+DATABASE_HOST = "localhost"  # ili IP adresa servera
+DATABASE_PORT = "5432"  # podrazumevani port za PostgreSQL
+DATABASE_NAME = "moja_baza"  # naziv baze podataka
+
+# URL za konekciju na PostgreSQL
+postgresql_url = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+
+# Kreiranje engine objekta za SQLModel
+engine = create_engine(postgresql_url)
